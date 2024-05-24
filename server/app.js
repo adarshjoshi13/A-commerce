313c79a15dc0c926f1abee007e8fe8e1a4555b58
Adarsh Joshi <adarshjoshi@referloan.in>
2024-05-24 18:41:17 +0530

    some more changes

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5000",
+  origin: ["http://localhost:5000", "https://a-commerce-seven.vercel.app/"],
   credentials: true,
   optionsSuccessStatus: 200
 }));
